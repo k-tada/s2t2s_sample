4bf4b61ae6d1a162eb7f7197b83827d25d756034
多田 健太郎 <k-tada@tada-no-MacBook-Air.local>
2016-04-15 16:07:57 +0900

    Localサーバで動作するよう修正

---
+++ gulp/tasks/connect.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: './www/',
+    port: 9080,
+    livereload: true
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ var requireDir = require('require-dir');
 
 requireDir('./gulp/tasks', { resource: true });
 
-gulp.task('watch', ['webpack', 'copy'], function() {
+gulp.task('watch', function() {
   gulp.watch('./src/js/**/*.js', ['webpack']);
   gulp.watch('./src/js/**/*.css', ['webpack']);
   gulp.watch('./src/*.html', ['copy']);
+  gulp.start(['connect'])
 });
 
 gulp.task('build', ['copy', 'webpack']);
